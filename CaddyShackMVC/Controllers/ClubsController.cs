c4f744a04f5b2eae80c0a52edb23b658bdb42c3b
bradenasmith2 <bradenasmith@icloud.com>
2023-08-03 11:51:16 -0500

    completed assessment

---
+++ CaddyShackMVC/Controllers/ClubsController.cs
@@ -0,0 +1,37 @@
+﻿using CaddyShackMVC.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using CaddyShackMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaddyShackMVC.Controllers
+{
+    public class ClubsController : Controller
+    {
+        private readonly CaddyShackContext _context;
+
+        public ClubsController(CaddyShackContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/golfbags/{id:int}/clubs/new")]
+        public IActionResult New(int id)
+        {
+            var gBag = _context.GolfBags.Include(gBag => gBag.Clubs).Where(e => e.Id == id).Single();
+
+            return View(gBag);
+        }
+
+        [HttpPost]
+        [Route("/golfbags/{id:int}/clubs")]
+        public IActionResult Index(int id, Club club)
+        {
+            var gBag = _context.GolfBags.Include(gBag => gBag.Clubs).Where(e => e.Id == id).Single();
+
+            gBag.Clubs.Add(club);
+            _context.SaveChanges();
+
+            return Redirect($"/golfbags/{gBag.Id}"); 
+        }
+    }
+}
---
+++ CaddyShackMVC/Controllers/GolfBagsController.cs
@@ -0,0 +1,75 @@
+﻿using CaddyShackMVC.DataAccess;
+using CaddyShackMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaddyShackMVC.Controllers
+{
+    public class GolfBagsController : Controller
+    {
+        private readonly CaddyShackContext _context;
+
+        public GolfBagsController(CaddyShackContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var gBags = _context.GolfBags.ToList();
+            ViewData["clubs"] = _context.Clubs.ToList();
+
+            return View(gBags);
+        }
+
+        [Route("/golfbags/{id:int}")]
+        public IActionResult Show(int id)
+        {
+            var gBag = _context.GolfBags.Include(e => e.Clubs).Where(e => e.Id == id).Single();
+            return View(gBag);
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(GolfBag gBag)
+        {
+            _context.GolfBags.Add(gBag);
+            _context.SaveChanges();
+
+            return Redirect($"/golfbags/{gBag.Id}");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var gBag = _context.GolfBags.Find(id);
+
+            return View(gBag);
+        }
+
+        [HttpPost]
+        [Route("/golfbags/{id:int}")]
+        public IActionResult Update(int id, GolfBag gBag)
+        {
+            gBag.Id = id;
+            _context.GolfBags.Update(gBag);
+            _context.SaveChanges();
+
+            return Redirect($"/golfbags/{gBag.Id}");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var gBag = _context.GolfBags.Find(id);
+            _context.GolfBags.Remove(gBag);
+            _context.SaveChanges();
+
+            return Redirect("/golfbags");
+        }
+    }
+}
